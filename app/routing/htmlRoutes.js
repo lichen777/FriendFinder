2f68cf3e9036e1c5cc731d28d7f2b15ef9eda5c6
Lichen <LC@Lichens-MacBook-Air.local>
2018-02-04 13:13:44 -0600

    AJAX function working

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,15 +1,21 @@
+var path = require('path')
+
 module.exports = function (router) {
   // middleware that is specific to this router
   router.use(function timeLog (req, res, next) {
     console.log('Time: ', Date.now())
     next()
   })
-  // define the home page route
-  router.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../public/home.html'))
-  })
   // define the survey route
   router.get('/survey', function (req, res) {
     res.sendFile(path.join(__dirname, '../public/survey.html'))
+  })
+  // define the home page route
+  router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'))
+  })
+
+  router.get('*', function (req, res) {
+    res.redirect('/')
   })
 }
