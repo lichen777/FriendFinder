cfa3f9c1d7fc8faefac5019aacfa08bd098c939f
Lichen <LC@Lichens-MacBook-Air.local>
2018-02-06 20:32:09 -0600

    server PORT updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
 
-var PORT = process.env.port || 3000
+var PORT = process.env.PORT || 3000
 
 // parse application/x-www-form-urlencoded and application/JSON
 app.use(bodyParser.urlencoded({ extended: false }))
